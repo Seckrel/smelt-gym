39b92f0df93f50840b8221d0e5ad80aadd7ade5f
Aayam <aayam.oza@gmail.com>
2022-01-28 19:32:54 +0545

    first commit of the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+import { CssBaseline } from '@mui/material';
+
+const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
+
 
 export default MyApp
---
+++ src/components/ShowCase/index.js
@@ -0,0 +1,5 @@
+export default function ShowCase() {
+    return(
+        <div></div>
+    )
+}
